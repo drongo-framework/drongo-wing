a4aaa9b8c25f93570452f13a5cd352613f8169ca
Sattvik Chakravarthy <sattvik@gmail.com>
2017-03-22 22:55:19 +0530

    Refactoring session module.

--- feathers/session/backend.py
+++ feathers/session/backend.py
@@ -1,11 +1,9 @@
 from drongo.utils import dict2
 
 import pickle
-import uuid
 
 
 DEFAULT = pickle.dumps(dict2())
-SESSION_COOKIE = '_drongo_sessid'
 
 
 class Backend(object):
@@ -17,13 +15,10 @@ class RedisBackend(Backend):
         self.app = app
         self.connection = connection
 
-    def load(self, request):
-        sessid = request.cookies.get(SESSION_COOKIE, None) or uuid.uuid4().hex
-        request.context.session = pickle.loads(self.connection.get(sessid)
-                                               or DEFAULT)
-        request.context.session._id = sessid
+    def load(self, session_id):
+        session = pickle.loads(self.connection.get(session_id) or DEFAULT)
+        session._id = session_id
+        return session
 
-    def save(self, request, response):
-        session = request.context.session
+    def save(self, session):
         self.connection.set(session._id, pickle.dumps(session))
-        response.set_cookie(SESSION_COOKIE, session._id)
--- feathers/session/module.py
+++ feathers/session/module.py
@@ -1,5 +1,10 @@
 from .backend import RedisBackend
 
+import uuid
+
+
+SESSION_COOKIE = '_drongo_sessid'
+
 
 class Session(object):
     def __init__(self, app, backend='redis', connection=None):
@@ -9,9 +14,12 @@ class Session(object):
             self.backend = RedisBackend(app, connection)
 
     def load(self, request):
+        sessid = request.cookies.get(SESSION_COOKIE, None) or uuid.uuid4().hex
         if self.backend is not None:
-            self.backend.load(request)
+            request.context.session = self.backend.load(sessid)
+            return request.context.session
 
     def save(self, request, response):
         if self.backend is not None:
-            self.backend.save(request, response)
+            self.backend.save(request.context.session)
+            response.set_cookie(SESSION_COOKIE, request.context.session._id)

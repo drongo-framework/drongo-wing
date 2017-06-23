1c6b7abe43f809090f1da5ac5b3e0212c1c8a517
Sattvik Chakravarthy <sattvik@gmail.com>
2017-06-23 20:13:14 +0530

    Adding template decorator for the jinja module.

--- wing-jinja2/wing_jinja2/module.py
+++ wing-jinja2/wing_jinja2/module.py
@@ -15,6 +15,23 @@ class Jinja2(object):
             loader=jinja2.FileSystemLoader(self.root_dir),
             undefined=SilentUndefined
         )
+        self.app.add_middleware(self)
 
     def get_template(self, name):
         return self.env.get_template(name)
+
+    def after(self, ctx):
+        if '__drongo_template' in ctx:
+            ctx.response.set_content(
+                self.get_template(ctx['__drongo_template']).render(ctx))
+
+    @classmethod
+    def template(cls, name):
+        def _inner1(method):
+            def _inner2(*args, **kwargs):
+                ctx = args[-1]
+                ctx['__drongo_template'] = name
+                result = method(*args, **kwargs)
+                return result
+            return _inner2
+        return _inner1
